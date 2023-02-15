63c7c62ae1a15559c4aa993427831a5017c61333
oscarg11 <oscar.g2511@gmail.com>
2023-02-14 22:01:18 -0500

    Created a birthday button that will increase one person's age by 1 using useState

--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 // this function accepts props (prarameters) that can be reused for different instances of PersonCard
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor} = props;
+            //getter, setter
+    const [ personAge, setPersonAge ] = useState(age);
+    // add age function
+    const addAge = () => {
+        setPersonAge(personAge + 1)
+    }
     return (
         <div>
             <h1>{lastName}, { firstName }</h1>
-            <p>age: { age }</p>
+            <p>age: { personAge }</p>
             <p>Hair Color: { hairColor }</p>
+            {/* increases the persons age by 1 */}
+            <button onClick={addAge}>Birday Button for {firstName} {lastName}</button>
+
+            {/* another way without using the addAge method */}
+            {/* <button onClick={(event) => setPersonAge(personAge +1)}>Birday Button for {firstName} {lastName}</button> */}
         </div>
     )
 }
