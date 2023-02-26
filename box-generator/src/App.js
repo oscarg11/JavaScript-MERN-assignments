71d5acfde01f5a3293c8e7aef569ab6cea37139b
oscarg11 <oscar.g2511@gmail.com>
2023-02-26 16:29:58 -0500

    form component that accepts a color and prints a box of the appropriate color when submitted

---
+++ box-generator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxGen from './components/boxGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxGen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ box-generator/src/components/boxGenerator.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+// const boxStyle = {
+//     display: "inline-block",
+//     height: "200px",
+//     width: "200px",
+//     border: "2px solid black",
+//     backgroundColor: "box.boxColor"
+// };
+// box object function
+const BoxGen = () => {
+    const [box, setBox] = useState({
+        boxColor: ""
+    })
+
+    // (box list) array of box objects
+    const [boxList, setBoxList] = useState([])
+
+    // targets and updates box attributes based on what key the user changes on the form
+    const onChangeHandler = (e) => {
+        setBox({...box, [e.target.name]: e.target.value})
+    }
+
+    const onSubmitHandler = (e) => {
+        // prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+        console.log(`BoxGen: ${JSON.stringify(box)}`)
+        // add to our box list with all box attribute changes applied( just the color for now)
+        setBoxList([...boxList, box])
+    }
+    return (
+        <div>
+            <form action="" className="form col-md-4 mx-auto" onSubmit={ onSubmitHandler}>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">Type any Color:</label>
+                    <input type="text" name="boxColor" className="form-control" onChange={ onChangeHandler} value={ box.boxColor }/>
+                    {/* button */}
+                    <button className="btn btn-primary mt-3">Submit</button>
+                </div>
+            </form>
+            {/* dispaly boxes */}
+            <div>
+                {
+                    boxList.map((box, i) => (
+                        <div key={i} style={{
+                            display: "inline-block",
+                            height: "200px",
+                            width: "200px",
+                            margin: "5px",
+                            border: "2px solid black",
+                            backgroundColor: box.boxColor
+                        }}>
+                        </div>
+                        
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+export default BoxGen
