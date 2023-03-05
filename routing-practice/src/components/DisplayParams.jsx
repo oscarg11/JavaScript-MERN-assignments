e24dad8a315578864cea7058daa7f5a566889d6a
oscarg11 <oscar.g2511@gmail.com>
2023-03-05 17:52:42 -0500

    added helpful comments to the DisplayParams.jsx component

--- routing-practice/src/components/DisplayParams.jsx
+++ routing-practice/src/components/DisplayParams.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-const DisplayParams = (props) => {
+const DisplayParams = () => {
     const { numOrword, color, backgroundCol } = useParams();
     return(
         <div>
+          {/* ternary operator to check if numOrword is a word or number */}
+          {/* if true word is printed  */}
+
           {
             isNaN(numOrword)?
+            // (Ternary operator for conditional styling)
+            //styles are applied only if color and backgroundCol are also inputs 
             <p style={
               color?
               {color: color, backgroundColor: backgroundCol, fontSize: "2.5em"}
@@ -14,11 +19,9 @@ const DisplayParams = (props) => {
             }>
               The word is : { numOrword }
             </p>
-          // if numOrWord is a number then 
+          // if numOrWord is a number then... 
           :
-          <p>
-            The number is { numOrword }
-          </p>
+          <p>The number is: { numOrword }</p>
         }
         </div>
       )
